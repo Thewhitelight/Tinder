6db9c91ba14fa08fc6d148b471d846c42d0d97c5
shizhiqiang <szq921618920@gmai..com>
2018-08-17 09:21:24 +0800

    add jCenter

--- app/src/main/java/cn/libery/tinder/MainActivity.kt
+++ app/src/main/java/cn/libery/tinder/MainActivity.kt
@@ -4,8 +4,8 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import cn.libery.avatar.Avatar
 import cn.libery.badgeview.BadgeView
-import cn.libery.selectavatar.Avatar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
--- avatar/src/test/java/cn/libery/avatar/ExampleUnitTest.java
+++ avatar/src/test/java/cn/libery/avatar/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package cn.libery.selectavatar;
+package cn.libery.avatar;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
