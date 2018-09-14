5929dc7d6d7e534e0e7ef3ca4b673161b6ba97d2
shizhiqiang <szq921618920@gmai..com>
2018-09-14 11:18:29 +0800

    update avatar

--- avatar/src/main/java/cn/libery/avatar/util/ImageUtil.java
+++ avatar/src/main/java/cn/libery/avatar/util/ImageUtil.java
@@ -5,10 +5,10 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.media.ExifInterface;
 import android.text.TextUtils;
 
 import java.io.File;
